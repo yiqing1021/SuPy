59ece9e54a1881c91854175ca52fb3a50880d6b6
Ting Sun <sunting.05@gmail.com>
2021-02-17 10:29:27 +0000

    update to driver 2020b14

--- src/supy/test/test_SuPy.py
+++ src/supy/test/test_SuPy.py
@@ -29,7 +29,12 @@ class TestSuPy(TestCase):
         df_output, df_state = sp.run_supy(
             df_forcing_part, df_state_init, save_state=True
         )
-        test_non_empty = np.all([not df_output.empty, not df_state.empty,])
+        test_non_empty = np.all(
+            [
+                not df_output.empty,
+                not df_state.empty,
+            ]
+        )
         self.assertTrue(test_non_empty)
 
     # test if multi-tstep mode can run
@@ -50,7 +55,12 @@ class TestSuPy(TestCase):
         #     # Now works as before.
         #     # print("Captured:\n", capturedOutput.getvalue())
 
-        test_non_empty = np.all([not df_output.empty, not df_state.empty,])
+        test_non_empty = np.all(
+            [
+                not df_output.empty,
+                not df_state.empty,
+            ]
+        )
         self.assertTrue(test_non_empty)
 
     # test if multi-grid simulation can run in parallel
@@ -62,7 +72,12 @@ class TestSuPy(TestCase):
         df_forcing_part = df_forcing_tstep.iloc[: 288 * 60]
         df_output, df_state = sp.run_supy(df_forcing_part, df_state_init)
 
-        test_success_sim = np.all([not df_output.empty, not df_state.empty,])
+        test_success_sim = np.all(
+            [
+                not df_output.empty,
+                not df_state.empty,
+            ]
+        )
 
         with tempfile.TemporaryDirectory() as dir_temp:
             list_outfile = sp.save_supy(
@@ -94,12 +109,20 @@ class TestSuPy(TestCase):
     def test_is_supy_euqal_mode(self):
         df_state_init, df_forcing_tstep = sp.load_SampleData()
         df_forcing_part = df_forcing_tstep.iloc[: 288 * 1]
+
+        # output group for testing
+        list_grp_test = [
+            "SUEWS",
+            "DailyState",
+            "RSL",
+        ]
+
         # single-step results
         df_output_s, df_state_s = sp.run_supy(
             df_forcing_part, df_state_init, save_state=True
         )
         df_res_s = (
-            df_output_s.loc[:, ["SUEWS", "DailyState", "snow",]]
+            df_output_s.loc[:, list_grp_test]
             .fillna(-999.0)
             .sort_index(axis=1)
             .round(6)
@@ -112,7 +135,7 @@ class TestSuPy(TestCase):
             df_forcing_part, df_state_init, save_state=False
         )
         df_res_m = (
-            df_output_m.loc[:, ["SUEWS", "DailyState", "snow",]]
+            df_output_m.loc[:, list_grp_test]
             .fillna(-999.0)
             .sort_index(axis=1)
             .round(6)
@@ -120,7 +143,8 @@ class TestSuPy(TestCase):
         )
         # print(df_res_m.iloc[:3, 86], df_res_s.iloc[:3, 86])
         pd.testing.assert_frame_equal(
-            left=df_res_s, right=df_res_m,
+            left=df_res_s,
+            right=df_res_m,
         )
         # test_equal_mode = df_res_s.eq(df_res_m).all(None)
         # self.assertTrue(test_equal_mode)
@@ -192,7 +216,7 @@ class TestSuPy(TestCase):
             force_download=False,
         )
         df_forcing = sp.util.read_suews(list_fn_fc[0])
-        ser_tair=df_forcing.Tair
+        ser_tair = df_forcing.Tair
         # ds_sfc = xr.open_mfdataset(list_fn_sfc)
         # ser_t2 = ds_sfc.t2m.to_series()
         # res_dif = ((df_forcing.Tair + 273.15 - ser_t2.values) / 98).round(4)
