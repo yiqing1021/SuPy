08b99d9505d976d21ab315a64cb57f8b98f3ab3a
Ting Sun <sunting.05@gmail.com>
2022-05-17 11:33:33 +0100

    add "simple" and "full" modes to `show_version`

--- src/supy/_version.py
+++ src/supy/_version.py
@@ -12,13 +12,14 @@ __version__ = f"{ser_ver.ver_milestone}.{ser_ver.ver_major}.{ser_ver.ver_minor}{
 __version_driver__ = sd_ver
 
 
-def show_version():
+def show_version(mode="simple"):
     """print `supy` and `supy_driver` version information.
     """
     print("SuPy versions")
     print("-------------")
     print(f"supy: {__version__}")
     print(f"supy_driver: {__version_driver__}")
-    print("\n=================")
-    print("SYSTEM DEPENDENCY")
-    pd.show_versions()
+    if mode == "full":
+        print("\n=================")
+        print("SYSTEM DEPENDENCY")
+        pd.show_versions()
